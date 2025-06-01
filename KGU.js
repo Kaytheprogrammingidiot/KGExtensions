927c770ab5c5514f0630bbf6b59f460112d13bf0
KayGamez <angela.thompson19622@gmail.com>
2025-06-01 01:50:32 -0400

    Create KGU.js

---
+++ KGU.js
@@ -0,0 +1,67 @@
+if (!Scratch.extensions.unsandboxed) {
+    throw new Error("KAYGAMEZ Utils must be run unsandboxed");
+}
+const vm = Scratch.vm;
+
+  let deltaTime = 0;
+  let previousTime = 0;
+
+  vm.runtime.on("BEFORE_EXECUTE", () => {
+    const now = performance.now();
+
+    if (previousTime === 0) {
+      deltaTime = 1 / vm.runtime.frameLoop.framerate;
+    } else {
+      deltaTime = (now - previousTime) / 1000;
+    }
+
+    previousTime = now;
+  });
+
+
+class KAYGAMEZUtils {
+    getInfo(){
+        return {
+            id: 'kaygamezutils',
+            name: 'KAYGAMEZ Utils',
+            color1: '#cad0d9',
+            color2: '#1600ff',
+            color3: '#1600ff',
+            blocks: [
+                {
+                    opcode: 'percentof',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: '[PERCENT]% of [NUMBER]',
+                    arguments:{
+                        PERCENT:{
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 1
+                        },
+                        NUMBER:{
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 100
+                        }
+                    }
+                        },
+                {
+                     opcode: 'moveusingdelta',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: '[STEPS] steps using delta time',
+                    arguments:{
+                        STEPS:{
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 10
+                }        
+                    }
+                }
+            ]
+        };
+    }
+    percentof(args) {
+        return args.PERCENT / 100 * args.NUMBER;
+    }
+    moveusingdelta(args){
+        return args.STEPS * 20 * deltaTime
+    }
+}
+Scratch.extensions.register(new KAYGAMEZUtils());
